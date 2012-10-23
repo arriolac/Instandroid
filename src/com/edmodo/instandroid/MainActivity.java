64ae3252b2b2c9c26af4cbc0851c4e312ce622fc
Chris Arriola <arriolac279@gmail.com>
2012-10-22 23:29:40 -0700

    Getting image from camera.

--- src/com/edmodo/instandroid/MainActivity.java
+++ src/com/edmodo/instandroid/MainActivity.java
@@ -1,15 +1,47 @@
 package com.edmodo.instandroid;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 
+    private final static int CAMERA_CAPTURE = 1;
+    
+    private ImageView mPicView;
+    private Bitmap mPicBitmap;
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        mPicView = (ImageView) findViewById(R.id.image_content);
+        Button btnCamera = (Button) findViewById(R.id.btn_camera);
+        btnCamera.setOnClickListener(new OnClickListener() {
+            
+            @Override
+            public void onClick(View v) {
+                Intent captureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(captureIntent, CAMERA_CAPTURE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Successfully retrieved image from camera
+        if (requestCode == CAMERA_CAPTURE && resultCode == RESULT_OK) {
+            mPicBitmap = (Bitmap) data.getExtras().get("data");
+            mPicView.setImageBitmap(mPicBitmap);
+        }
     }
 
     @Override
